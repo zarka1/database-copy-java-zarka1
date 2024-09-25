79f3dd8b5ce1bf0e552aa398eb481d87a521af5a
zarka1 <szucsbalazs367@gmail.com>
2024-09-25 16:59:02 +0200

    Initial commit

---
+++ src/main/java/com/example/databaseCopyAndAnalyze/DatabaseCopyAndAnalyzeApplication.java
@@ -0,0 +1,13 @@
+package com.example.databasecopy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabasecopyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabasecopyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/databaseCopyAndAnalyze/DatabasecopyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.databasecopy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabasecopyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
